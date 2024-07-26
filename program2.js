5139b6d26f63e0aaf66b4a8b51495bc706ae2809
shiviChouhan02 <shivani.m0214@gmail.com>
2024-07-26 00:56:20 -0700

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,7 +1,32 @@
 const decodeTheRing = function (s, p) {
 
-    // write your code here
+  let sIndex = 0;
+  let pIndex = 0;
 
-  };
-  
-  module.exports = decodeTheRing;
+  while (sIndex < s.length && pIndex < p.length) {
+    if (p[pIndex] === '*') {
+
+      if (pIndex === p.length - 1) return true;
+
+      let nextChar = p[pIndex + 1];
+      let nextCharIndex = s.indexOf(nextChar, sIndex);
+
+      if (nextCharIndex === -1) return false;
+
+      
+
+      sIndex = nextCharIndex;
+      pIndex++;
+    } else if (p[pIndex] === '?' || p[pIndex] === s[sIndex]) {
+      sIndex++;
+      pIndex++;
+    } else {
+      return false;
+    }
+  }
+
+  return sIndex === s.length && pIndex === p.length;
+
+};
+
+module.exports = decodeTheRing;
